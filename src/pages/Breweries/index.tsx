5987fff1171f51da87a4a0200c5c9f7f0d66fefd
Drayton80 <drayton80@hotmail.com>
2022-07-17 15:15:27 -0300

    Breweries: Creation of the Breweries Page

---
+++ src/pages/Breweries/index.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { BREWERIES } from "../../constants/idNames";
+import { URL_LOGIN } from "../../constants/urlRoutes";
+import { useNavigate } from "react-router";
+import BackButton from "../../components/BackButton";
+import breweriesService from "../../services/Breweries";
+import BreweryCard from "../../components/BreweryCard";
+import { useUserStore } from "../../stores/User/Context";
+import "./style.less";
+import { BreweryInfo } from "../../services/Breweries/types";
+
+const Breweries: React.FC = () => {
+  const navigate = useNavigate();
+  const userStore = useUserStore();
+  const [breweries, setBreweries] = useState<BreweryInfo[]>([]);
+
+  useEffect(() => {
+    breweriesService.getAll().then((data) => setBreweries(data.data));
+  }, []);
+
+  const handleCardDelete = (event: React.FormEvent<HTMLButtonElement>) => {
+    const breweriesUpdated = [...breweries];
+    const breweryIndex = breweries
+      .map((brewery) => brewery.id)
+      .indexOf(event.currentTarget.value);
+
+    breweriesUpdated.splice(breweryIndex, 1);
+
+    setBreweries(breweriesUpdated);
+  };
+
+  return (
+    <>
+      <header id={BREWERIES}>
+        <nav>
+          <BackButton onClick={() => navigate(URL_LOGIN)} />
+        </nav>
+        <h2>{userStore?.user.name}</h2>
+      </header>
+      <main id={BREWERIES}>
+        <section>
+          {breweries.map((brewery) => (
+            <BreweryCard brewery={brewery} onDeleteClick={handleCardDelete} />
+          ))}
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default Breweries;
