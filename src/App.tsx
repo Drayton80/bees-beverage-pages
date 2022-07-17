e7ccee2195b6983edfa36e57070d541740a746a2
Drayton80 <drayton80@hotmail.com>
2022-07-17 15:09:47 -0300

    App: Adding constants and Breweries Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login/index";
+import { URL_LOGIN, URL_BREWERIES } from "./constants/urlRoutes";
+import Breweries from "./pages/Breweries";
+import Login from "./pages/Login";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="login" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/breweries" element={<></>} />
+        <Route path="/" element={<Navigate to={URL_LOGIN} />} />
+        <Route path={URL_LOGIN} element={<Login />} />
+        <Route path={URL_BREWERIES} element={<Breweries />} />
       </Routes>
     </BrowserRouter>
   );
