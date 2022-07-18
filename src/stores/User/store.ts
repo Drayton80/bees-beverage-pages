8a03305f1832d9dd989efa00a71f98480b58e3d1
Drayton80 <drayton80@hotmail.com>
2022-07-17 22:43:22 -0300

    Store: Refactoring actions and Breweries in User store

--- src/stores/User/store.ts
+++ src/stores/User/store.ts
@@ -1,9 +1,9 @@
-import { observable, runInAction } from "mobx";
+import { action, observable, runInAction } from "mobx";
 import { BreweryInfo } from "services/Breweries/types";
 import { User, UserStore } from "./types";
 
 export const createUserStore = (): UserStore => {
-  const state = observable({
+  const state: User = observable({
     name: "",
     breweries: [],
   });
@@ -13,15 +13,47 @@ export const createUserStore = (): UserStore => {
     set(user: User) {
       this.user = { ...user };
     },
-    setName(name: string) {
-      runInAction(() => {
-        this.user.name = name;
-      });
-    },
-    setBreweries(breweries: BreweryInfo[]) {
-      runInAction(() => {
-        this.user.breweries = breweries;
-      });
-    },
+    setName: action((state: User, name: string) => {
+      state.name = name;
+    }),
+    setBreweries: action((state: User, breweries: BreweryInfo[]) => {
+      state.breweries =
+        breweries.map((brewery) => ({
+          ...brewery,
+          customTags: ["add more"],
+        })) ?? [];
+    }),
+    deleteBrewery: action((state: User, breweryId: string) => {
+      const breweryIndex = state.breweries
+        .map((brewery) => brewery.id)
+        .indexOf(breweryId);
+      state.breweries.splice(breweryIndex, 1);
+    }),
+    pushCustomTag: action((state: User, breweryId: string, tag: string) => {
+      const breweryIndex = state.breweries
+        .map((brewery) => brewery.id)
+        .indexOf(breweryId);
+      state.breweries[breweryIndex]?.customTags?.push(tag);
+    }),
+    editCustomTag: action(
+      (
+        state: User,
+        breweryId: string,
+        tagIndex: number,
+        tagNewText: string
+      ) => {
+        const breweryIndex = state.breweries
+          .map((brewery) => brewery.id)
+          .indexOf(breweryId);
+
+        if (tagNewText === "") state.breweries.splice(breweryIndex, 1);
+
+        if (
+          tagNewText !== "" &&
+          state?.breweries[breweryIndex]?.customTags[tagIndex]
+        )
+          state.breweries[breweryIndex].customTags[tagIndex] = tagNewText;
+      }
+    ),
   };
 };
